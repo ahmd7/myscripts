11e54bebeba62b18ff6346c6b3b7b044c3988baf
Ahmed Samir <ahmed.s.abdelrahim7@gmail.com>
2023-11-30 04:17:55 +0200

    done

--- deploystaticwebsite/websitedeploy.sh
+++ deploystaticwebsite/websitedeploy.sh
@@ -10,28 +10,52 @@ then
 	SRV='httpd'
 	sudo rm -rf $TEMP
 	sudo rm -rf /var/www/html/*
+	echo "##########################"
+	echo "start Installing packages"
+	echo "##########################"
 	sudo yum install $PACK $SRV -y &> /dev/null
+	echo "##########################"
+        echo "Packages installed"
+        echo "##########################"
 	sudo systemctl start $SRV
 	sudo systemctl enable $SRV
 	mkdir -p $TEMP
 	cd $TEMP
+	echo "##########################"
+        echo "Downloading and unziping source code"
+        echo "##########################"
 	sudo wget $URL &> /dev/null
 	sudo unzip $ZIP &> /dev/null
 	sudo cp $LOC/* /var/www/html &> /dev/null
 	sudo systemctl restart httpd
+	echo "##########################"
+  	echo "your website is up and running"
+        echo "##########################"
 	sudo rm -rf $TEMP
 else
 	sudo rm -rf $TEMP
 	SRV='apache2'
-    sudo mv /var/www/html/* /dev/null
-    sudo apt update
+    	sudo mv /var/www/html/* /dev/null
+    	sudo apt update
+	echo "##########################"
+        echo "start Installing packages"
+        echo "##########################"
 	sudo apt install $PACK $SRV -y &> /dev/null
-    sudo systemctl start $SRV
+	echo "##########################"
+        echo "Packages installed"
+        echo "##########################"
+    	sudo systemctl start $SRV
 	sudo systemctl enable $SRV
 	mkdir $TEMP
 	cd $TEMP
+	echo "##########################"
+        echo "Downloading and unziping source code"
+        echo "##########################"
 	sudo wget $URL &> /dev/null
 	sudo unzip $ZIP &> /dev/null 
+	echo "##########################"
+        echo "your website is up and running"
+        echo "##########################"
 	sudo cp $LOC/* /var/www/html &> /dev/null
 	sudo systemctl restart $SRV
 	sudo rm -rf $TEMP
